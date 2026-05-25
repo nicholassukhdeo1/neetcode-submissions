347940d7b22841c952b29df17afc636ca688f0d3
nicholassukhdeo1 <nicholassukhdeo1@gmail.com>
2026-05-25 15:25:57 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        
+        
+        letterMap = {}
+
+        for currLetter in s:
+
+            if currLetter not in letterMap:
+
+                letterMap[currLetter] = 1
+
+            else:
+
+                letterMap[currLetter] += 1
+
+        for currLetter in t:
+
+            if currLetter not in letterMap:
+
+                letterMap[currLetter] = 1
+                return False
+
+            else:
+
+                letterMap[currLetter] -= 1
+
+        for currLetter in t:
+            if letterMap[currLetter] != 0:
+                return False
+        
+
+        return True
+
+
+    #then, we need to compare the hashmaps.
+    #yeah, what if i make another variable to store the
+
+        
