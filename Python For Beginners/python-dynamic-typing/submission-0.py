1fa9a7f11c9ba9b6be193009fd2888914b886dca
nicholassukhdeo1 <nicholassukhdeo1@gmail.com>
2026-05-28 10:53:59 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,21 @@
+
+
+variable = 12
+
+print(type(variable))
+
+variable = 12.0
+
+print(type(variable))
+
+variable = True
+
+print(type(variable))
+
+variable = "Hi"
+
+print(type(variable))
+
+variable = [1,2,3]
+
+print(type(variable))
