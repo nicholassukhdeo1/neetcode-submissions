0f91cf8b7d4b5ace1e3842e3d901c14bd4e49ca2
Nicholas Sukhdeo <nicholassukhdeo1@gmail.com>
2026-09-18 20:15:56 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+
+        # only take in alphanumeric
+
+        res = []
+
+        for L in range(len(s)):
+            if (s[L] >= 'a' and s[L] <= 'z') or (s[L] >= 'A' and s[L] <= 'Z'):
+                to_input = (s[L]).lower()
+                res.append(to_input)
+            elif (s[L] >= '0' and s[L] <= '9'):
+                res.append(s[L])
+
+        R = len(res) - 1
+
+        print(res)
+
+        for L in range(len(res)):
+            if res[L] != res[R]:
+                return False
+            R -= 1
+
+        return True
